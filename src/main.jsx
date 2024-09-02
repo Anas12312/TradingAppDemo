ce32a7f8e6c5e5d5958175b5f54fff4e71822139
unknown <zyad.sallem.007@gmail.com>
2024-09-02 19:18:05 +0300

    z

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import LogIn from './Components/LogIn.jsx'
+
+
+const Main = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  return (
+    <>
+      {
+        isLoggedIn ? 
+        <App /> :
+        <LogIn setIsLoggedIn={setIsLoggedIn}/>
+      }      
+    </>
+  );
+}
+
+export default Main;
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
 )
